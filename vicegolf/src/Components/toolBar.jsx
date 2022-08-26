de54a7c0ea3f7253b92f2689537bd4dcf4a38d82
unknown <codyt11@hotmail.com>
2022-08-25 21:51:45 -0500

    cleaned up react app and added html for tool bar

---
+++ vicegolf/src/Components/toolBar.jsx
@@ -0,0 +1,23 @@
+import {React, useState} from "react";
+
+
+function ToolBar() {
+    
+    return(
+        <div>
+            <select>"Country"</select>
+            <h3>ORDER BY PHONE</h3>
+            <div>
+                <div>
+                    <h1>Vice</h1>
+                    <h1>Cart</h1>
+                </div>
+                <div>
+                    0
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ToolBar
