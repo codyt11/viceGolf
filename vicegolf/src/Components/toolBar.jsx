7345e42d10d8208a1d735086a09064499d365c7d
unknown <codyt11@hotmail.com>
2022-09-27 00:34:32 -0500

    top tool bar rough

--- vicegolf/src/Components/toolBar.jsx
+++ vicegolf/src/Components/toolBar.jsx
@@ -1,19 +1,30 @@
 import {React, useState} from "react";
-
+import './toolBar.css';
+import Flag from '../images/USA.svg';
+import Phone from '../images/phone.svg';
 
 function ToolBar() {
     
     return(
-        <div>
-            <select>"Country"</select>
-            <h3>ORDER BY PHONE</h3>
-            <div>
-                <div>
-                    <h1>Vice</h1>
-                    <h1>Cart</h1>
+        <div className="text-center bg-light" >
+            <div className="row d-flex justify-content-between w-100 topRow">
+                <div className="col-4 h-100 d-flex justify-content-between align-items-center">
+                    <div className="selector align-items-center d-flex">
+                        <select className="test bg-light"></select>
+                        <img className="flag" src={Flag} alt="" />
+                        <h4 className="country">USA</h4>
+                    </div>
+                    <div className="order d-flex w-100 mx-5 align-items-center">
+                        <img className="phone mx-1" src={Phone} alt="" />
+                        <a className="order text-decoration-none text-dark">ORDER BY PHONE</a>
+                    </div>
                 </div>
-                <div>
-                    0
+                <div className="col-3 cart d-flex h-100">
+                    <div className="title d-flex flex-column">
+                        <div className="fst-italic vice">VICE</div>
+                        <div className="fs-5 fw-bold cart">CART</div>
+                    </div>
+                    <h1 className="number">0</h1>
                 </div>
             </div>
         </div>
